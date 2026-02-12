eec6be41831622bd7c2c30f99991d9f906ffaa57
xb0616 <2780318633@qq.com>
2026-02-12 20:38:52 +0000

    4.2 finished
    successful load image

--- rango/tango_with_django_project/tango_with_django_project/settings.py
+++ rango/tango_with_django_project/tango_with_django_project/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR / 'templates'
+STATIC_DIR = BASE_DIR / 'static'
+
 
 
 
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
