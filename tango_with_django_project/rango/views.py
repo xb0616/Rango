695b8d8f5f5c14f25f1097e941215590f4dc4a13
xb0616 <2780318633@qq.com>
2026-02-13 02:44:52 +0000

    chapter10 finished
    pass all the chapter 10 test

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,6 +7,28 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -18,16 +40,26 @@ def index(request):
     context_dict['categories'] = category_list
 
     context_dict['pages'] = page_list
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
-    context_dict['visits'] = num_visits
+    # num_visits = request.session.get('num_visits', 0)
+    # request.session['num_visits'] = num_visits + 1
+    visitor_cookie_handler(request)
+    # context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
 
+
+    # response = render(request, 'rango/index.html', context=context_dict)
+    # visitor_cookie_handler(request, response)
+    # return response
+    # context_dict['visits'] = request.session['visits']
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    num_visits = request.session.get('num_visits', 0)
-    context_dict = {'boldmessage': 'This is the about page.'}
-    context_dict['visits'] = num_visits
+    # num_visits = request.session.get('num_visits', 0)
+    # context_dict = {'boldmessage': 'This is the about page.'}
+    # context_dict['visits'] = num_visits
+    # return render(request, 'rango/about.html', context=context_dict)
+    visitor_cookie_handler(request)         
+    visits = request.session['visits']    
+    context_dict = {'visits': visits}
     return render(request, 'rango/about.html', context=context_dict)
 
 
